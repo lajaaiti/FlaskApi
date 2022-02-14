8e71f6531ac0aff74c0ec8e781d258a0b296ea61
L.A <lajaaiti.anouar@gmail.com>
2022-02-14 11:05:36 +0100

    mise a jour

--- exoFlaskApi/api.py
+++ exoFlaskApi/api.py
@@ -1,12 +1,16 @@
 #import flask
-from flask import Flask, request, jsonify, template_rendered
+from flask import Flask, template_rendered
 
 # creation de l'application
 app = Flask(__name__)
 
 # définition de la route
 @app.route('/')
-def index():
-    re
+def home():
+    return template_rendered('index.html')
+
+
+
+    
     
 
