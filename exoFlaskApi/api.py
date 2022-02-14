c5dd8d8a124ab5d234e57fe3308002aecd268d17
L.A <lajaaiti.anouar@gmail.com>
2022-02-14 10:15:09 +0100

    venv 01

--- exoFlaskApi/api.py
+++ exoFlaskApi/api.py
@@ -1,1 +1,10 @@
+#import flask
+from flask import Flask, request, jsonify
 
+# creation de l'application
+app = Flask(__name__)
+
+# définition de la route
+@app.route('/')
+def index():
+    return "Hello World !"
